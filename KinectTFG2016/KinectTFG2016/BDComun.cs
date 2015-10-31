85a92313d164076e62f2a135847f1e1624f89dcb
David López Santamaria <dls0010@alu.ubu.es>
2015-10-31 12:29:08 +0100

    Añadida clase BDComun, que permite el acceso a la BD local "KinectBD" donde se encuentran las tablas necesarias para el funcionamiento del programa.

---
+++ KinectTFG2016/KinectTFG2016/BDComun.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace KinectTFG2016
+{
+    class BDComun
+    {
+        public static SqlConnection ObtnerConexion()
+        {
+            SqlConnection Conn = new SqlConnection(@"Data source=DAVID-PORTATIL\DAVID; Initial Catalog = KinectBD; Integrated Security=True");
+            Conn.Open();
+
+            return Conn;
+        }
+    }
+}
